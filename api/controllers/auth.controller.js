43dfc6ef1add8f4abb564b013c321c9964e15cf5
Vikram <srisaivikram24982498@gmail.com>
2025-01-15 19:37:29 +0530

    hashed password,create middleware and a function to handle errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,12 +1,15 @@
 import User from '../models/user.models.js';
-export const signup=async(req,res)=>{
+import bcryptjs from 'bcryptjs';
+export const signup=async(req,res,next)=>{
     const {username,email,password}=req.body;
-    const newUser=new User({username,email,password});
+    const hashedPassword=bcryptjs.hashSync(password,10);
+    const newUser=new User({username,email,password:hashedPassword});
     try{
         await newUser.save();//because it takes few seconds to save in the database
         res.status(201).json("User create successfully!");
     } catch(err){
-        res.status(500).json(err.message);
+        // res.status(500).json(err.message);//we use middleware instead of this because there are many api endpoints we cant write or handle error for every api endpoint so we use api endpoint
+        next(err);// sending err to middleware
     }
     
 };
