281e11328d4687b94b39c55e5589eab2445b9d34
Vikram <srisaivikram24982498@gmail.com>
2025-01-18 15:10:23 +0530

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from '../models/user.models.js';
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utilis/error.js';
+import jwt from 'jsonwebtoken';
 export const signup=async(req,res,next)=>{
     const {username,email,password}=req.body;
     const hashedPassword=bcryptjs.hashSync(password,10);
@@ -10,6 +12,22 @@ export const signup=async(req,res,next)=>{
     } catch(err){
         // res.status(500).json(err.message);//we use middleware instead of this because there are many api endpoints we cant write or handle error for every api endpoint so we use api endpoint
         next(err);// sending err to middleware
-    }
-    
+    }  
+};
+export const signin=async(req,res,next)=>{
+    const {email,password}=req.body;
+    try{
+        const validUser=await User.findOne({email});
+        if (!validUser) return next(errorHandler(404,'User not found!'));
+        const validPassword=bcryptjs.compareSync(password,validUser.password);
+        if (!validPassword) return next(errorHandler(401,'Wrong credientials'));
+        const token = jwt.sign({id: validUser._id} , process.env.JWT_SECRET);
+        const {password:pass , ...rest}= validUser._doc;
+        res
+            .cookie('access_token',token, {httpOnly:true})
+            .status(200)
+            .json(rest);
+    } catch(err){
+        next(err);
+    }  
 };
