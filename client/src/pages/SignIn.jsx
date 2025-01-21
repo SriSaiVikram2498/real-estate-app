7dea60ad2527b3956c867f805a674b746a058574
Vikram <srisaivikram24982498@gmail.com>
2025-01-21 23:22:29 +0530

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,39 +1,51 @@
 import React, { useState } from 'react'
 import {Link,useNavigate} from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { signInStart,signInSuccess,signInFailure } from '../redux/user/userSlice';
 export default function SignIn() {
   const [formData,setFormData]=useState({});
-  const [error,setError] = useState(null);
-  const [loading,setLoading] = useState(false);
+  // const [error,setError] = useState(null);
+  // const [loading,setLoading] = useState(false);
+  const {loading,error} = useSelector((state) => state.user);//in userSlice the state name was user
   const navigate=useNavigate();
+  const dispatch = useDispatch();
   const handleChange = (e) =>{
     setFormData({
       ...formData,
       [e.target.id]:e.target.value,
     });
     console.log(formData);
-  }
+  };
   const handleSubmit = async (e) =>{
     e.preventDefault();//this prevent the page from reloading 
-    setLoading(true);
-    const res= await fetch('/api/auth/signin',
-      {
-        method:'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData),
+    // setLoading(true);
+    try{
+      dispatch(signInStart());
+      const res= await fetch('/api/auth/signin',
+        {
+          method:'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(formData),
+        }
+      );
+      const data = await res.json();
+      if (data.success === false){
+        // setError(data.message);
+        // setLoading(false);
+        dispatch(signInFailure(data.message));
+        return;
       }
-    );
-    const data = await res.json();
-    if (data.success === false){
-      setError(data.message);
-      setLoading(false);
-      return;
-    }
-    setLoading(false);
-    setError(null);
-    navigate('/');
-    console.log(data);
+      // setLoading(false);
+      // setError(null);
+      dispatch(signInSuccess(data));
+      navigate('/');
+    } catch(error){
+      // setLoading(false);
+      // setError(error.message);
+      dispatch(signInFailure(error.message));
+    } 
   };
   return (
     <div className='p-3 max-w-lg mx-auto'>
